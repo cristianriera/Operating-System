d29fe260414af2fd816f3a361074ad02d2eb777e
cristianriera <7criera@stpatrick.org>
2016-10-13 10:26:53 -0500

    Add files via upload
    Made new apps

--- AppObject.js
+++ AppObject.js
@@ -1,4 +1,4 @@
-function App(){
+function App(n,m,s){
 
     //Declare your instance variables here.
     this.name = n;
@@ -7,27 +7,37 @@ function App(){
 
     //Declare the instance function open here.
     this.open = function(){
-      this.state = "active"
+      this.state = "active";
 
     };
     //Declare the instance function sleep here.
     this.sleep = function(){
-      
-
+      if(this.state == "active"){
+        this.state = "sleep";
+        this.memory = (this.memory)/2;
+      }
+    };
     //Declare the instance function active here.
     this.active = function(){
-
-
+      if(this.state == "sleep"){
+        this.state = "active";
+        this.memory = (this.memory)*2;
+      }
+    };
     //Declare the instance function close here.
-    this.active = function(){}
-
-
-
+    this.close = function(){
+      if(this.state != null){
+        this.state = null;
+        this.memory = 0
+      }
+    };
 }
 
 //Use the constructor to create 4 App objects below.
-
-
-
+var Crisapp = new app()
+var myapp = new app()
+var yourapp = new app()
+var thisapp = new app()
+var appList = [Crisapp,myapp,yourapp,thisapp]
 //Declare an array named appList, and place all 4 of your apps in that array.
 //The order does not matter.
